b7b392b4d73d77f1e7a9d60d60ca19d976ff458e
Sanyok <supergulman09@gmail.com>
2020-10-23 13:29:11 +0300

    Init

---
+++ database/seeds/PermissionSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $manageUser = new Permission();
+        $manageUser->name = 'Manage Users';
+        $manageUser->slug = 'manage-users';
+        $manageUser->save();
+
+        $createTasks = new Permission();
+        $createTasks->name = 'Create Tasks';
+        $createTasks->slug = 'create-tasks';
+        $createTasks->save();
+    }
+}
---
+++ resources/views/pm.blade.php
@@ -0,0 +1,1 @@
+<h1>PROJECT MANAGER</h1>
